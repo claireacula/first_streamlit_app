2448e83df51a80a714af5a361f4d56a3f006e07f
claireacula <136769777+claireacula@users.noreply.github.com>
2023-06-16 10:40:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.txt('Breakfast Menu')
+streamlit.text('Breakfast Menu')
 
 streamlit.text('omega 3 & blueberry outmeal')
 
