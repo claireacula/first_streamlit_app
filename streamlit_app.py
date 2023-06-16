85195f283d56b5e38268a798e4da964824cd4b99
claireacula <136769777+claireacula@users.noreply.github.com>
2023-06-16 10:38:26 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit
+
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.txt('Breakfast Menu')
+
+streamlit.text('omega 3 & blueberry outmeal')
+
+streamlit.text('Kale Spinach & Rocket Smothie')
+
+streamlit.text('Hard boiled Free-range egg')
