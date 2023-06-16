b625a57d94998de43d3deea9b34322762ca033ec
claireacula <136769777+claireacula@users.noreply.github.com>
2023-06-16 10:18:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-import streamlit
-
 streamlit.menu('Breakfast Menu')
-
-import streamlit
 
 streamlit.foods('omega 3 & blueberry outmeal')
